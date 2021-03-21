b2140ee10ff5aafc92892574c33616eb61f65ccc
Dallen Pyrah <dallenpyrah@gmail.com>
2021-03-21 16:00:13 -0600

    So good

--- bug-logger.client/src/components/Bug.vue
+++ bug-logger.client/src/components/Bug.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="col-12">
-    <div class="card bug animate__animated animate__fadeInRight shadow-sm m-2 text-center p-5 rounded-bug bg-dark text-light">
+  <div class="col-12 animate__animated animate__fadeInRight">
+    <div class="card bug  shadow-sm m-2 text-center p-5 rounded-bug bg-dark text-light">
       <router-link :to="{ name: 'BugDetailsPage', params: {id: bug.id}}">
         <h3 class="text-light">
           {{ bug.title }}
